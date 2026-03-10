2e8d84a02943c0de7b24afd32934d1df2f337ba7
Mayzihardiputra <eziee496@gmail.com>
2026-03-10 13:52:57 +0700

    Add project files.

---
+++ AkademikADOApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AkademikADOApp
+{
+    public partial class Form1 : Form
+    {
+        string connString = "Data Source=MAYZIHARDIPUTRA\\MAYZIHARDIPUTRA;initial Catalog=DBAkademikADO;Integrated Security=True";
+        SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(connString);
+                conn.Open();
+
+                lblStatus.Text = "Status : Database Connected";
+                MessageBox.Show("Koneksi ke database berhasil!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal : " + ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    lblStatus.Text = "Status : Database Disconnected";
+                    MessageBox.Show("Koneksi ke database ditutup!");
+                }
+                else
+                {
+                    MessageBox.Show("Database belum terkoneksi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menutup koneksi : " + ex.Message);
+            }
+        }
+    }
+}
