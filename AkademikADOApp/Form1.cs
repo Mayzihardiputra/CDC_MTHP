dd71a9a5cb77477bf42fbffefce62128cc5b477c
Mayzihardiputra <eziee496@gmail.com>
2026-03-10 14:08:44 +0700

    commit 2

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace AkademikADOApp
+namespace AkademikADOApp // Namespace untuk aplikasi Akademik ADO.NET
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form // Kelas Form1 yang merupakan form utama aplikasi
     {
         string connString = "Data Source=MAYZIHARDIPUTRA\\MAYZIHARDIPUTRA;initial Catalog=DBAkademikADO;Integrated Security=True";
         SqlConnection conn;
 
-        public Form1()
+        public Form1() // Konstruktor untuk Form1
         {
             InitializeComponent(); // Inisialisasi komponen form
         }
